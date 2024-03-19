c8dc7aeeac44d6fb3a79d9ea0b7ba776d4a4fd7f
Sandeep chetnani <chetnansandeep@gmail.com>
2024-03-19 21:46:05 +0530

    CHANES

---
+++ Name.java
@@ -0,0 +1,18 @@
+class Name{
+public static void main(String args[])
+{
+int n1=87
+int n2=301;
+for(int i=45;i<=n1;i++){
+for(int j=201;l<=n2;j++){
+if(
+i==63 && j>=201 && j<=206 || i==63 && j>=211 && j<=217||i==63 && j>=222 && j<=225||i==63 && j>=229 && j<=236||i==63 && j>=240 && j<=244||i==63 && j>=248 && j<=266||i==63 && j>=270 && j<=277||i==63 && j>=282 && j<=300||
+
+
+
+)
+{System.out.print("@");}
+else{System.out.print(" ");}
+
+}
+System.out.println();}
